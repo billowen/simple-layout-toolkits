b81c2331acdd05edea9cdbf5fe0aede94ccf23e0
KP Shao <kp.shao@outlook.com>
2019-07-23 20:35:57 +0800

    Add Cell class

---
+++ src/gds/cell.h
@@ -0,0 +1,20 @@
+#ifndef GDS_CELL_H
+#define GDS_CELL_H
+
+namespace gds {
+class Layout;
+/**
+ * @brief A collection of drawing elements and references to other Cells
+ */
+class Cell
+{
+public:
+    Cell(Layout *parent);
+};
+
+}
+
+
+
+
+#endif // CELL_H
--- src/gds/gds.pri
+++ src/gds/gds.pri
@@ -1,5 +1,7 @@
 HEADERS += \
+    $$PWD/cell.h \
     $$PWD/layout.h
 
 SOURCES += \
+    $$PWD/cell.cpp \
     $$PWD/layout.cpp
