a5bb2cab2c3bbb423e2566411d8cad01873f3220
KP Shao <kp.shao@outlook.com>
2019-07-23 23:30:22 +0800

    Implement Cell

--- src/gds/cell.cpp
+++ src/gds/cell.cpp
@@ -1,1 +1,42 @@
 #include "cell.h"
+
+gds::Cell::Cell(gds::Layout *parent)
+{
+    _parent = parent;
+    _name = "";
+}
+
+gds::Layout *gds::Cell::parent() const
+{
+    return _parent;
+}
+
+QString gds::Cell::name() const
+{
+    return _name;
+}
+
+QVector<gds::ElementBase *> gds::Cell::elements() const
+{
+    QVector<gds::ElementBase *> list;
+    for (auto &ptr : _elements) {
+        list.push_back(ptr.get());
+    }
+
+    return list;
+}
+
+QVector<gds::ReferenceBase *> gds::Cell::references() const
+{
+    QVector<gds::ReferenceBase *> list;
+    for (auto &ptr : _references) {
+        list.push_back(ptr.get());
+    }
+
+    return list;
+}
+
+void gds::Cell::setName(const QString &name)
+{
+    _name = name;
+}
--- src/gds/cell.h
+++ src/gds/cell.h
@@ -1,5 +1,9 @@
 #ifndef GDS_CELL_H
 #define GDS_CELL_H
+#include <QString>
+#include <memory>
+#include "elementbase.h"
+#include "referencebase.h"
 
 namespace gds {
 class Layout;
@@ -10,6 +14,29 @@ class Cell
 {
 public:
     Cell(Layout *parent);
+
+    Layout * parent() const;
+    QString name() const;
+    /**
+     * @brief Get all drawing elements.
+     * @return List of drawing elements.
+     */
+    QVector<ElementBase*> elements() const;
+    /**
+     * @brief Get all references.
+     * @return List of references;
+     */
+    QVector<ReferenceBase*> references() const;
+
+    void setName(const QString &name);
+
+private:
+    Layout *_parent;
+    QString _name;
+
+    std::vector<std::unique_ptr<ElementBase> > _elements;
+    std::vector<std::unique_ptr<ReferenceBase> > _references;
+
 };
 
 }
